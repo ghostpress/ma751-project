0b1a555feceff07db277946884bd3eec26b217d6
Lucia V <luciamvila@gmail.com>
2025-04-21 18:22:09 -0400

    Added documentation to functions

--- src/models/lasso.py
+++ src/models/lasso.py
@@ -1,11 +1,26 @@
 import numpy as np
 
 class LassoRegression():
+    """Class to implement LASSO regression, based on (3.52) in Hastie's 'Elements of Statistical Learning' (Springer, 2013).
+    The model is trained using gradient descent. 
+    """
+
     def __init__(self, iterations=10000):
         self.iterations=iterations
         pass
 
+
     def fit(self, X, y, reg, lr=0.01):
+        """Function to fit LASSO model, using gradient descent on the intercept and feature parameters.
+        
+        Parameters
+        ------
+        X : numpy.ndarray : column data
+        y : numpy.ndrarry : target data
+        reg : float : regularization term
+        lr : float : learning rate 
+        """
+
         self.M, self.N = X.shape
         self.B = np.zeros(self.N)
         self.B_0 = 0
@@ -19,7 +34,7 @@ class LassoRegression():
 
         for k in range(self.iterations):
             if k % 1000 == 0:
-                print(f"Iteration: {k}/{self.iterations}")
+                print(f"Iteration: {k}/{self.iterations}")  # give a status update every 100 iterations
                 self.gradient_descent_update(status=True)
             else:
                 self.gradient_descent_update()
@@ -27,7 +42,15 @@ class LassoRegression():
         print(f"Updated values: \nB_0: {self.B_0} \nB: {self.B}")
         return self
 
+
     def gradient_descent_update(self, status=False):
+        """Function to perform gradient-descent updates on the LASSO parameters. 
+
+        Parameters
+        ----------
+        status : bool : whether to print a status update on the parameter values and MSE in a given iteration.
+        """
+
         yhat = self.predict(self.X)
         gradB = np.zeros(self.N)
         residual = np.subtract(self.y, yhat)
@@ -50,6 +73,14 @@ class LassoRegression():
             print(f"B0: self.B_0 \nB: {self.B}")
         return self
 
+
     def predict(self, X):
+        """Function to predict values of the target variable given features X and LASSO parameters.
+
+        Parameters
+        ----------
+        X : numpy.ndarray : feature data with which to predict
+        """
+
         return(X.dot(self.B) + self.B_0)
 
